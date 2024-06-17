774960e991522406e6c84914a25fa754c0702a63
Atharva Patil <152093228+kaabilengineer@users.noreply.github.com>
2024-06-18 00:11:34 +0530

    day1.py

---
+++ day1.py
@@ -0,0 +1,15 @@
+# 1071. Greatest Common Divisor of Strings
+class Solution(object):
+    def gcdOfStrings(self, str1, str2):
+        """
+        :type str1: str
+        :type str2: str
+        :rtype: str
+        """
+        if str1 + str2 != str2 + str1:
+            return ""
+        if len(str1) == len(str2):
+            return str1
+        if len(str1) > len(str2):
+            return self.gcdOfStrings(str1[len(str2):], str2)
+        return self.gcdOfStrings(str1, str2[len(str1):])
